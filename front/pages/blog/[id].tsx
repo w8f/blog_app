d6ea99c3c122edbb03c8a28bb9aba31dbeb12c6e
w8f <haya22aws@gmail.com>
2022-01-03 10:36:19 +0900

    シンタックスハイライト、記事ページのデザイン追加。

--- front/pages/blog/[id].tsx
+++ front/pages/blog/[id].tsx
@@ -23,16 +23,21 @@ type Props = {
 
 const BlogId: NextPage<Props> = ({ blog, highlightedBody }) => {
   return (
-    <main>
-      <h1>{blog.title}</h1>
-      <p>{blog.publishedAt}</p>
-      <p>{blog.category && `${blog.category.name}`}</p>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `${highlightedBody}`,
-        }}
-      />
-    </main>
+    <div className="bg-gray-200">
+      <main className="sm:container sm:mx-auto mx-auto min-h-screen flex-1 overflow-x-hidden">
+        <div className="text-center m-8">
+          <h1 className="text-4xl font-bold m-4">{blog.title}</h1>
+          <p>{blog.publishedAt.match(/\d+-\d+-\d+/) + "に公開"}</p>
+          <p>{blog.category && `${blog.category.name}`}</p>
+        </div>
+        <div
+          className="bg-white sm:p-12 md:p-16 lg:p-20 xl:p-24 shadow"
+          dangerouslySetInnerHTML={{
+            __html: `${highlightedBody}`,
+          }}
+        />
+      </main>
+    </div>
   );
 };
 
@@ -56,6 +61,40 @@ export const getStaticProps: GetStaticProps = async (context) => {
     const result = hljs.highlightAuto($(el).text());
     $(el).html(result.value);
     $(el).addClass("hljs");
+  });
+
+  $("h1").each((_, el) => {
+    $(el).addClass("text-4xl m-1");
+  });
+
+  $("h2").each((_, el) => {
+    $(el).addClass(
+      "text-3xl border-solid border-4 border-blue-300 bg-blue-200 m-2 "
+    );
+  });
+
+  $("h3").each((_, el) => {
+    $(el).addClass("text-2xl m-3 underline");
+  });
+
+  $("a").each((_, el) => {
+    $(el).addClass("ml-4 mr-4 mb-4 text-blue-800");
+  });
+
+  $("p").each((_, el) => {
+    $(el).addClass("ml-4 mr-4 mb-4");
+  });
+
+  $("ul").each((_, el) => {
+    $(el).addClass("ml-12 pd-1 list-disc");
+  });
+
+  $("ol").each((_, el) => {
+    $(el).addClass("ml-12 pd-1 list-decimal");
+  });
+
+  $("blockquote").each((_, el) => {
+    $(el).addClass("border-l-8 ml-4 mr-4 bg-gray-50");
   });
 
   return {
