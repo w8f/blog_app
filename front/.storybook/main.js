b236b826e1e215c256583b1cee71be3bc59c8bd7
w8f <haya22aws@gmail.com>
2022-02-11 21:27:26 +0900

    fix:#7 storybookの画面にtailwindcssが反映されるよう修正

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -1,3 +1,5 @@
+import "../styles/globals.css";
+
 module.exports = {
   typescript: { reactDocgen: false },
   stories: [
@@ -5,7 +7,18 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
   framework: "@storybook/react",
   core: {
     builder: "webpack5",
