8b0533dec92982791d5bb64e6e2a7af643cf9c4f
w8f <haya22aws@gmail.com>
2022-02-12 14:48:51 +0900

    add:ArticleCardのストーリ追加。 ・storybook上で、NextImageタグをimgタグに置換する設定変更

--- front/.storybook/preview.js
+++ front/.storybook/preview.js
@@ -1,4 +1,10 @@
+import * as nextImage from "next/image";
 import "../styles/globals.css";
+
+Object.defineProperty(nextImage, "default", {
+  configurable: true,
+  value: (props) => <img {...props} />,
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
---
+++ front/components/ArticleCard/ArticleCard.stories.tsx
@@ -0,0 +1,38 @@
+import ArticleCard from "../ArticleCard/ArticleCard";
+import { Blog } from "../../interfaces/index";
+import { Meta, Story } from "@storybook/react";
+
+export default {
+  component: ArticleCard,
+  title: "articleCard",
+} as Meta;
+
+const Template: Story<Blog> = (args) => <ArticleCard {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  id: "1",
+  title: "default blog title",
+  publishedAt: "2022-02-01",
+  updatedAt: "2022-02-01",
+  body: "<h1>hogehoge</h1>",
+  categories: [
+    {
+      name: "hoge",
+      image: {
+        url: "https://images.microcms-assets.io/assets/797f5c01c5ba4a20a0888ddda0fecbe1/f2cbae8993c44d0896f8792496416b59/nextjsimage.png",
+        height: "100",
+        width: "100",
+      },
+    },
+    {
+      name: "fuga",
+      image: {
+        url: "https://images.microcms-assets.io/assets/797f5c01c5ba4a20a0888ddda0fecbe1/f2cbae8993c44d0896f8792496416b59/nextjsimage.png",
+        height: "100",
+        width: "100",
+      },
+    },
+  ],
+};
