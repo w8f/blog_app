051169b8ac26fdb6d537cee2cb9924151573382b
w8f <haya22aws@gmail.com>
2022-02-12 13:45:53 +0900

    fix:#8 Footer、背景色の表示範囲を統一。Footerの枠追加。

--- front/components/Footer/Footer.tsx
+++ front/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer>
+    <footer className="border-t-2">
       <h1 className="text-center mt-4 mb-2 bg-white font-serif">
         © 2022 Koichi Hayakawa All rights reserved.
       </h1>
