680e0da616eb1bae85617851c0629ee4f2732013
w8f <haya22aws@gmail.com>
2022-02-19 21:01:50 +0900

    mod: Headerの要素をレスポンシブに変更

--- front/components/Header/Header.tsx
+++ front/components/Header/Header.tsx
@@ -6,15 +6,19 @@ import Link from "next/link";
 
 const Header: VFC = () => {
   return (
-    <div className="bg-white border-b-2 h-14 flex items-center">
-      <Link href="/">
-        <a className="hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-4 sm:ml-8 z-10">
-          Hayakawa&apos;s Tech Blog
+    <div className="bg-white border-b-2 h-14 flex">
+      <div className="lg:w-2/12" />
+      <div className="lg:w-6/12 flex items-center">
+        <Link href="/">
+          <a className="hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-4 sm:ml-8 z-10">
+            Hayakawa&apos;s Tech Blog
+          </a>
+        </Link>
+        <a href="https://github.com/w8f/blog_app">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
-      </Link>
-      <a href="https://github.com/w8f/blog_app">
-        <FontAwesomeIcon icon={faGithub} size="2x" />
-      </a>
+      </div>
+      <div className="lg:w-2/12" />
     </div>
   );
 };
