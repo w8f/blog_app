9e0800e46e0fbde4a72651bee60dc25a4d0160ef
w8f <haya22aws@gmail.com>
2022-04-05 20:25:33 +0900

    mod: Remove unnecessary css

--- front/components/ArticleCard/ArticleCard.tsx
+++ front/components/ArticleCard/ArticleCard.tsx
@@ -10,7 +10,7 @@ const ArticleCard: VFC<Blog> = ({
   categories,
 }: Blog) => {
   return (
-    <article className="text-gray-800 m-3 max-w-md">
+    <article className="text-gray-800 m-3 items-center">
       <Link href={`/blog/${id}`} passHref>
         <a className="rounded-2xl overflow-hidden bg-white items-center m-4 flex h-full hover:shadow-xl hover:ease-in duration-300 hover:text-blue-800 border-2">
           <div className="w-full">
